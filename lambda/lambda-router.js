8f703207f2f9e651366281c93491bb09b6672b03
unknown <stanj89@gmail.com>
2019-07-09 21:03:15 -0400

    fully functional

--- lambda/lambda-router.js
+++ lambda/lambda-router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const Lambda = require('../data/db.js')
+const Posts = require('../data/db.js')
 
 const router = express.Router();
 
@@ -9,8 +9,8 @@ router.use(express.json());
 //GET all posts
 router.get('/', async (req, res) => {
     try {
-        const lambda = await Lambda.find();
-        res.status(200).json(lambda)
+        const post = await Posts.find();
+        res.status(200).json(post)
     } catch (err) {
         console.log(err)
         res.status(500).json({
@@ -22,10 +22,10 @@ router.get('/', async (req, res) => {
 //GET specific post
 router.get('/:id', async (req, res) => {
     try {
-        const lambda = await Lambda.findById(req.params.id);
+        const post = await Posts.findById(req.params.id);
 
-        if (lambda.length > 0) {
-            res.status(200).json(lambda)
+        if (post.length > 0) {
+            res.status(200).json(post)
         } else {
             res.status(404).json({ message: "The post with the specified ID does not exist" })
         }
@@ -36,13 +36,30 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post("/", async (req, res) => {
+//GET comments on a post
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const lambda = await Posts.findPostComments(req.params.id);
+
+        if (lambda.length > 0) {
+            res.status(200).json(lambda)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved."
+        })
+    }
+})
+
+//POST a new post
+router.post('/', async (req, res) => {
     if (req.body.title && req.body.contents) {
         try {
-            const lambda = await Lambda.insert(req.body);
-            Lambda.findById(lambda.id).then(post => {
-                res.status(200).json(post)
-            })
+            const lambda = await Posts.insert(req.body);
+            const lambdaID = await Posts.findById(lambda.id)
+            res.status(200).json(lambdaID)
         } catch (err) {
             res.status(500).json({
                 message: "There was an error while saving the post to the database"
@@ -54,5 +71,78 @@ router.post("/", async (req, res) => {
         })
     }
 })
+
+//POST a new comment
+router.post('/:id/comments', async (req, res) => {
+    const commentInfo = {
+        post_id: req.params.id,
+        text: req.body.text
+    }
+    if (req.body.text) {
+        try {
+            const lambda = await Posts.insertComment(commentInfo)
+            const lambdaComment = await Posts.findCommentById(lambda.id)
+            if (lambda) {
+                res.status(200).json(lambdaComment)
+            } else {
+                res.status(404).json({
+                    message: "The post could not be found"
+                })
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: "There was an error while saving the comment to the database"
+            })
+        }
+    } else {
+        res.status(400).json({
+            message: "Please provide text for the comment."
+        })
+    }
+})
+
+//DELETE a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const lambda = await Posts.findById(req.params.id)
+        const lambdaDelete = await Posts.remove(req.params.id)
+        if (lambdaDelete > 0) {
+            res.status(200).json(lambda)
+        } else {
+            res.status(404).json({
+                message: "The post could not be found"
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error removing the post'
+        })
+    }
+})
+
+//PUT update a post
+router.put('/:id', async (req, res) => {
+    if (req.body.title && req.body.contents) {
+        try {
+            const lambda = await Posts.update(req.params.id, req.body)
+            const lambdaID = await Posts.findById(req.params.id)
+            if (lambda) {
+                res.status(200).json(lambdaID)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: "The post information could not be modified."
+            })
+        }
+    } else res.status(400).json({
+        message: "Please provide title and contents for the post."
+    })
+})
+
+
 
 module.exports = router;
